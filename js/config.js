952e7c3dbc6a32539ff49d0cbfb0c1b60c94f285
刘玉华 <lyh@bensue.com>
2016-08-02 15:31:07 +0800

    修改申请发票页面

--- js/config.js
+++ js/config.js
@@ -38,7 +38,9 @@ var urls = {
         //发票管理
         getInvoiceList: urlAPI + '/trd/orderInfo/invoiceList', //可以开发票的订单列表
         //收货地址
-        saveDeliveryAddress: urlAPI + '/usr/deliveryAddress/save', //收货地址增加、修改
+        saveDeliveryAddress: urlAPI + '/usr/deliveryAddress/save', //收货地址增加、修改getInvoiceExpressFee
+        getDeliveryAddress: urlAPI + '/usr/deliveryAddress/get', //获取收货地址
+        getInvoiceExpressFee: urlAPI + '/usr/invoiceLog/invoiceExpressFee', //获取发票快递费用
         //房间相关
         getRoomTypeList: urlAPI + '/res/roomType/list', //房间类型列表
         getRoomList: urlAPI + '/res/room/list', //房间列表
@@ -445,6 +447,11 @@ function fmoney(value) {
 //判断是否为空
 function isEmpty(obj) {
     return obj === "" || obj == null || obj == undefined;
+}
+
+//判断是否为空
+function isNotEmpty(obj) {
+    return !isEmpty(obj);
 }
 //拼接picker字符串通用方法
 // function mapList(json,key1,key2){
--- js/pages/user/vmodel-invoice-apply.js
+++ js/pages/user/vmodel-invoice-apply.js
@@ -8,54 +8,98 @@ var vmInvoiceApply = avalon.define({
     oids: [],
     invoiceType: 1,
     payType: 2,
-    name: '',
-    mobile: '',
-    fullAddress: '',
+    title: '',
+    invoiceExpressFee: 0,
+    editDeliveryAddress: {
+        name: '',
+        mobile: '',
+        fullAddress: ''
+    },
+    defaultDeliveryAddress: {
+        id: '',
+        name: '',
+        mobile: '',
+        fullAddress: ''
+    },
+    getInvoiceExpressFee: function () {
+        ajaxJsonp({
+            url: urls.getInvoiceExpressFee,
+            successCallback: function (json) {
+                if (json.status == 1) {
+                    vmInvoiceApply.invoiceExpressFee = json.data;
+                } else {
+                    alert(json.message);
+                }
+            }
+        });
+    },
     openDeliveryAddress: function () {
+        vmInvoiceApply.editDeliveryAddress.name = vmInvoiceApply.defaultDeliveryAddress.name;
+        vmInvoiceApply.editDeliveryAddress.mobile = vmInvoiceApply.defaultDeliveryAddress.mobile;
+        vmInvoiceApply.editDeliveryAddress.fullAddress = vmInvoiceApply.defaultDeliveryAddress.fullAddress;
         popover('delivery-address-add.html', 1);
     },
+    getDeliveryAddress: function () {
+        ajaxJsonp({
+            url: urls.getDeliveryAddress,
+            successCallback: function (json) {
+                if (json.status == 1) {
+                    if (isNotEmpty(json.data)) {
+                        vmInvoiceApply.defaultDeliveryAddress = json.data;
+                    }
+                } else {
+                    alert(json.message);
+                }
+            }
+        });
+    },
     saveDeliveryAddress: function () {
-        if (vmInvoiceApply.name == "") {
+        if (vmInvoiceApply.editDeliveryAddress.name == "") {
             alert("收货人姓名不能为空");
             return false;
         }
-        if (vmInvoiceApply.mobile == "") {
+        if (vmInvoiceApply.editDeliveryAddress.mobile == "") {
             alert("手机号不能为空");
             return false;
         }
-        if (vmInvoiceApply.fullAddress == "") {
+        if (vmInvoiceApply.editDeliveryAddress.fullAddress == "") {
             alert("收货地址不能为空");
             return false;
         }
-        if (vmInvoiceApply.name.length > 50) {
+        if (vmInvoiceApply.editDeliveryAddress.name.length > 50) {
             alert("名字不能大于50个字");
             return false;
         }
-        if (vmInvoiceApply.mobile.length < 11) {
+        if (vmInvoiceApply.editDeliveryAddress.mobile.length < 11) {
             alert("手机号不能小于11位");
             return false;
         }
-        if (vmInvoiceApply.mobile.length > 11) {
+        if (vmInvoiceApply.editDeliveryAddress.mobile.length > 11) {
             alert("手机号不能大于11位");
             return false;
         }
-        if (vmInvoiceApply.fullAddress.length > 255) {
+        if (vmInvoiceApply.editDeliveryAddress.fullAddress.length > 255) {
             alert("名字不能大于255个字");
             return false;
         }
+        vmInvoiceApply.defaultDeliveryAddress.name = vmInvoiceApply.editDeliveryAddress.name;
+        vmInvoiceApply.defaultDeliveryAddress.mobile = vmInvoiceApply.editDeliveryAddress.mobile;
+        vmInvoiceApply.defaultDeliveryAddress.fullAddress = vmInvoiceApply.editDeliveryAddress.fullAddress;
 
         ajaxJsonp({
             url: urls.saveDeliveryAddress,
             data: {
-                mobile: vmInvoiceApply.mobile,
-                name: vmInvoiceApply.name,
-                fullName: vmInvoiceApply.fullName
+                id: vmInvoiceApply.defaultDeliveryAddress.id,
+                mobile: vmInvoiceApply.defaultDeliveryAddress.mobile,
+                name: vmInvoiceApply.defaultDeliveryAddress.name,
+                fullAddress: vmInvoiceApply.defaultDeliveryAddress.fullAddress
             },
             successCallback: function (json) {
                 if (json.status !== 1) {
                     alert(json.message);
                     return;
                 } else {
+                    vmInvoiceApply.defaultDeliveryAddress = json.data;
                     $('#pop-text').empty();
                     $('.popover').addClass('popover-hide');
                     popover_ishide = true;
@@ -83,3 +127,6 @@ function load() {
 }
 
 load();
+
+vmInvoiceApply.getDeliveryAddress();
+vmInvoiceApply.getInvoiceExpressFee();
