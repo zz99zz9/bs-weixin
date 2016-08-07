1c59e70ba950b263f012582973b7fa0115ce0555
sun-mac <sunkinux@qq.com>
2016-08-07 15:35:53 +0800

    入住流程页样式修改

--- js/pages/order/vmodel-payend.js
+++ js/pages/order/vmodel-payend.js
@@ -1,15 +1,56 @@
-var orderData = Storage.get("orderData");
+var orderid = getParam("id");
+
+if (orderid != "") {
+    if (isNaN(orderid)) {
+        location.href = document.referrer || "index.html";
+    } else {
+        orderid = parseInt(orderid);
+    }
+} else {
+    location.href = "index.html";
+}
 
 var vmPayend = avalon.define({
 	$id: "payend",
 	data: {},
+    goHotelById: function(id) {
+        location.href = "hotel.html?id=" + id;
+    },
+    openNav: function(lat, lng, name, addr) {
+        stopSwipeSkip.do(function() {
+            if (isSuccess) {
+                wx.openLocation({
+                    latitude: lat, // 纬度，浮点数，范围为90 ~ -90
+                    longitude: lng, // 经度，浮点数，范围为180 ~ -180。
+                    name: name, // 位置名
+                    address: addr, // 地址详情说明
+                    scale: 26, // 地图缩放级别,整形值,范围从1~28。默认为最大
+                    infoUrl: 'ini.xin' // 在查看位置界面底部显示的超链接,可点击跳转
+                });
+            } else {
+                alert("微信接口配置注册失败，将重新注册");
+                registerWeixinConfig();
+            }
+        });
+    },
 	goOrder: function() {
-		location.href = "order.html?id=" + vmPayend.data.id;
+		location.href = "order.html?id=" + orderid;
 	}
 });
 
-if(orderData) {
-    vmPayend.data = orderData;
-} else {
-    location.href = "index.html";
-}
+ajaxJsonp({
+    url: urls.getOrderDetail,
+    data: { id: orderid },
+    successCallback: function(json) {
+        if (json.status === 1) {
+            //只有订单是未入住的状态，才能打开这个页面
+            if(json.data.status == 2) {
+                vmPayend.data = json.data;
+            } else {
+                location.href = document.referrer || "index.html";
+            }
+        }
+    }
+});
+
+registerWeixinConfig();
