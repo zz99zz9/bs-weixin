be58a26149215a5c8d2d1bbca7ff3b080b930fce
sun-mac <sunkinux@qq.com>
2016-11-10 16:04:40 +0800

    会员中心的成为VIP图片修改 公益管理导航页面调整 订当天全天房不再灰掉入住时间

--- js/pages/cms/nav.js
+++ js/pages/cms/nav.js
@@ -1,13 +1,17 @@
 var vmFoundation = avalon.define({
     $id: 'foundation',
     data: {
-        name: '杜氏助学公益基金',
-        des: '基金创始于2012年，从最初发起人个人资助特贫困大学生完成学业开始，迅速发展成苏北地区具有较高影响力的助学公益组织，秉承基金创始人杜玉莲女士“让每个孩子只少能够拥有受教育的机会”之理念，坚持资助必须有始有终，不为名利，只为能够保留孩子心中那一丝对未来的期望！',
-        img: '../img/commonweal/love.png',
-        amount: 0,
-        account: 0,
+        cnName: '',
+        enName: '',
+        brief: '',
+        introduction: '',
+        logoUrl: ''
+    },
+    accountData: {
+        totalDonateAmount: 0,
+        cashAmount: 0,
         numberStudent: 0,
-        numberSub: 0
+        numberAccount: 0
     },
     fid: 0,
     getFid: function() {
@@ -16,20 +20,14 @@ var vmFoundation = avalon.define({
             successCallback: function(json) {
                 if (json.status == 1) {
                     vmFoundation.fid = json.data.id;
-                    vmFoundation.data.name = json.data.cnName;
-                    vmFoundation.data.des = json.data.introduction;
-                    vmFoundation.data.img = urlAPINet + json.data.logoUrl;;
-
+                    vmFoundation.data = json.data;
+                    
                     ajaxJsonp({
                         url: urls.getFoundationAccount,
                         data: { fid: vmFoundation.fid },
                         successCallback: function(json) {
                             if (json.status == 1) {
-                                vmFoundation.data.account = json.data.cashAmount;
-                                vmFoundation.data.numberStudent = json.data.numberStudent;
-                                vmFoundation.data.amount = json.data.totalDonateAmount;
-                                vmFoundation.data.numberSub = json.data.numberAccount;
-
+                                vmFoundation.accountData = json.data;
                             } else {
                                 mui.alert(json.message);
                             }
